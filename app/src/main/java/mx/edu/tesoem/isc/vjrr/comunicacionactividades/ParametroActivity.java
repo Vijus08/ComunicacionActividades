18339d333185527054bc59278a3a4748831dd96b
Vijus08 <vic.vijus@gmail.com>
2020-09-28 21:21:08 -0500

    Initial commit

---
+++ app/src/main/java/mx/edu/tesoem/isc/vjrr/comunicacionactividades/ParametroActivity.java
@@ -0,0 +1,21 @@
+package mx.edu.tesoem.isc.vjrr.comunicacionactividades;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class ParametroActivity extends AppCompatActivity {
+
+    TextView lblSaludo;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_parametro);
+        lblSaludo = findViewById(R.id.lblSaludo);
+        Bundle parametro = getIntent().getExtras();
+        lblSaludo.setText("Bienvenido " + parametro.getString( "Nombre"));
+    }
+
+}
---
+++ app/src/main/java/mx/edu/tesoem/isc/vjrr/comunicacionactividades/frmActivity.java
@@ -0,0 +1,16 @@
+package mx.edu.tesoem.isc.vjrr.comunicacionactividades;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+
+public class frmActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_frm_actividad);
+    }
+
+}
